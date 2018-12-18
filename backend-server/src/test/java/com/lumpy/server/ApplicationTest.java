1e00692935037891bc8c0757a7571c7c38ba5644
Unknown <elfdude@yahoo.com>
2018-12-18 11:29:36 -0600

    add backend-server project

---
+++ backend-server/src/test/java/com/lumpy/server/ApplicationTest.java
@@ -0,0 +1,11 @@
+package com.lumpy.server;
+
+import org.junit.jupiter.api.Test;
+
+public class ApplicationTest {
+
+	@Test
+	public void testIt() {
+		Application.main(new String[] { "" });
+	}
+}
