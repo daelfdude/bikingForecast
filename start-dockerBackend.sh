224ace3f0030f26a8b6367045a1efdc299710d63
Unknown <elfdude@yahoo.com>
2018-12-20 17:22:00 -0600

    enable starting server as docker container

---
+++ start-dockerBackend.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+./gradlew :backend-server:buildImage
+id=`docker run -d -l bob -p 8080:8080 -i -t biking-forecast-backend-server:latest`
+echo ""
+echo "Ctrl+C to shutdown"
+echo ""
+sleep 2
+start "" "http://localhost:8080/first"
+docker container attach $id
+docker container prune -f --filter "label=bob"
