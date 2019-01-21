f986dff0464965e0409aa3526b4635a1fd847462
Unknown <elfdude@yahoo.com>
2019-01-21 10:50:56 -0600

    rounding off rough edges of scripts

--- start-backendServer.sh
+++ start-backendServer.sh
@@ -1,2 +1,7 @@
 #!/bin/bash
-./gradlew :backend-server:run
+echo "------------------------------------------"
+echo "Ctrl+c (or MacOs equiv) to stop"
+echo "App should open in browser tab momentarily"
+echo "------------------------------------------"
+echo ""
+(sleep 5; start "" "http://localhost:8080/first" ) | ./gradlew :backend-server:run
--- start-dockerBackend.sh
+++ start-dockerBackend.sh
@@ -1,10 +1,30 @@
 #!/bin/bash
-./gradlew :backend-server:buildImage
+set -e
+function cleanup {
+	echo ""
+    echo "------------------------------------------"
+	echo "Killing container"
+    echo "------------------------------------------"
+    docker container kill $id
+	echo ""
+    echo "------------------------------------------"
+	echo "Removing container"
+    echo "------------------------------------------"
+	docker container rm $id
+}
+trap cleanup EXIT
+echo "------------------------------------------"
+echo "Building application & Docker image"
+echo "------------------------------------------"
+./gradlew :backend-server:buildImage --console=plain # because 'rich' jacks up later echos on Cmder:bash 
+echo ""
+echo "------------------------------------------"
+echo "Starting application via Docker image"
+echo "------------------------------------------"
+echo "App should open in browser tab momentarily"
+echo "App logs should show below"
+echo "Ctrl+c to kill and remove container"
+echo ""
 id=`docker run -d -l biking-forecase-local-dev -p 8080:8080 -i -t biking-forecast-backend-server:latest`
-echo ""
-echo "Ctrl+C to shutdown"
-echo ""
-sleep 2
-start "" "http://localhost:8080/first"
+(sleep 5; start "" "http://localhost:8080/first") &
 docker container attach $id
-docker container prune -f --filter "label=biking-forecase-local-dev"
